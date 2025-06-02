021b0f6f4c9958848e6aa52579805d8c99314020
Mr-Grinder <aaa3122556@gmail.com>
2025-06-02 14:53:24 +0300

    Update webhook_server.py

--- Sherlock_TONs/webhook_server.py
+++ Sherlock_TONs/webhook_server.py
@@ -1,27 +1,18 @@
 import os
-from aiogram import Bot, Dispatcher
-from aiogram.enums import ParseMode
-from aiogram.fsm.storage.memory import MemoryStorage
-from aiogram.client.default import DefaultBotProperties
 from dotenv import load_dotenv
 from fastapi import FastAPI
 from aiogram.webhook.aiohttp_server import SimpleRequestHandler, setup_application
 
-# Імпортуємо router з твого коду (handlers і т.д.)
-from sherlock_tons import router
+# Імпортуємо router з основного коду 
+from sherlock_tons import router, bot, dp
 
 # Завантажуємо токен і дані Webhook
 load_dotenv()
 BOT_TOKEN = os.getenv("TG_BOT_TOKEN")
 WEBHOOK_PATH = "/webhook"
-WEBHOOK_SECRET = "tralala"  # можеш змінити
+WEBHOOK_SECRET = "tralala"  
 WEBHOOK_BASE = os.getenv("WEBHOOK_BASE")  
 WEBHOOK_URL = f"{WEBHOOK_BASE}{WEBHOOK_PATH}"
-
-# Створення бота та диспетчера
-bot = Bot(token=BOT_TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
-dp = Dispatcher(storage=MemoryStorage())
-dp.include_router(router)
 
 # FastAPI сервер
 app = FastAPI()
